41691e4520fad8b0edaa8d38f54e77e5e1054998
Billy <billyaraujo@gmail.com>
2019-11-16 17:53:18 +0000

    Fixed warnings.

--- trunk/src/geometry/GeoVertexList_Imp.hpp
+++ trunk/src/geometry/GeoVertexList_Imp.hpp
@@ -23,48 +23,46 @@ namespace geometry {
     template <typename Real>
     CGeoVertexList<Real>::~CGeoVertexList()
     {
-
         this->reset();
     }
 
     template <typename Real>
     void CGeoVertexList<Real>::reset()
     {
-
         m_vertices.clear();
     }
 
     template <typename Real>
     Integer CGeoVertexList<Real>::nbVertices()
     {
-
         return static_cast<Integer>(m_vertices.size());
     }
 
     template <typename Real>
     void CGeoVertexList<Real>::addVertex(CGeoCoordinate<Real>& aVertex)
     {
-
         m_vertices.push_back(aVertex);
     }
 
     template <typename Real>
     void CGeoVertexList<Real>::insertVertex(const Integer aVertexIndex, CGeoCoordinate<Real>& aVertex)
     {
-
         m_vertices.insert(m_vertices.begin() + aVertexIndex, aVertex);
     }
 
     template <typename Real>
     void CGeoVertexList<Real>::removeVertex(const Integer aVertexIndex)
     {
-
         m_vertices.erase(m_vertices.begin() + aVertexIndex);
     }
 
     template <typename Real>
     CGeoCoordinate<Real>& CGeoVertexList<Real>::vertex(const Integer aVertexIndex)
     {
+        if (m_vertices.size() == 0)
+        {
+            throw std::runtime_error("Vertex size = 0");
+        }
 
         return m_vertices[aVertexIndex % m_vertices.size()];
     }
@@ -72,14 +70,17 @@ namespace geometry {
     template <typename Real>
     void CGeoVertexList<Real>::removeDuplicates()
     {
-
         m_vertices.erase(std::unique(m_vertices.begin(), m_vertices.end()), m_vertices.end());
     }
 
     template <typename Real>
     void CGeoVertexList<Real>::removeCollinear(const Real aTolerance)
     {
-
+        if (m_vertices.size() == 0)
+        {
+            return;
+        }
+        
         Integer j = 1;
 
         for (Integer i = 1; i < static_cast<Integer>(m_vertices.size()) - 1; ++i) {
@@ -105,7 +106,6 @@ namespace geometry {
     template <typename Real>
     void CGeoVertexList<Real>::invert()
     {
-
         std::reverse(m_vertices.begin(), m_vertices.end());
     }
 }
