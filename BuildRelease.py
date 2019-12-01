5e437a5a9666878ba245effca535e071d1e1b6a2
bjaraujo <billyaraujo@gmail.com>
2019-12-01 11:12:38 +0000

    Updated build script.

--- BuildRelease.py
+++ BuildRelease.py
@@ -1,6 +1,7 @@
 
 import os
 import shutil
+from sys import platform
    
 # Read version info
 fi = open('trunk/src/version.h', 'r')
@@ -17,14 +18,15 @@ strNewVersion = str(intMajorNumber) + '.' + str(intMinorNumber) + '.' + str(intR
 # Increment version
 incVersionInput = input('Increment version number [Y/n]?')
 
-incVersion = True 
-if incVersionInput == 'n':
-    incVersion = False
+#incVersion = True 
+#if incVersionInput == 'n':
+#    incVersion = False
 
+incVersion = False
+    
 fo = open('trunk/src/version.h', 'w')
 
 for strLine in strLines:
-
     if 'APP_VERSION_INFO' in strLine.upper(): 
         strPrevVersion = strLine.split(' ')[-1].rstrip().replace('"', '')
                 
@@ -60,8 +62,12 @@ os.system('pause')
 
 if not os.path.exists('build'):
     os.mkdir('build')
-    
-os.system('cmake -Bbuild trunk -G "Visual Studio 16 2019" -A Win32 -DENIGMA_BUILD_UNIT_TESTS:BOOL=OFF -DENIGMA_BUILD_WRAPPERS_SWIG:BOOL=ON -DWRAP_SWIG_PYTHON:BOOL=ON -DWRAP_SWIG_CSHARP:BOOL=ON -DSWIG_EXECUTABLE=D:/Libraries/Swig/swigwin-4.0.1/swig.exe')
+
+if os.name == 'win32':
+    os.system('cmake -Bbuild trunk -G "Visual Studio 16 2019" -A Win32 -DENIGMA_BUILD_UNIT_TESTS:BOOL=OFF -DENIGMA_BUILD_WRAPPERS_SWIG:BOOL=ON -DWRAP_SWIG_PYTHON:BOOL=ON -DWRAP_SWIG_CSHARP:BOOL=ON -DSWIG_EXECUTABLE=D:/Libraries/Swig/swigwin-4.0.1/swig.exe')
+else:
+    os.system('cmake -Bbuild trunk -G "Ninja" -DENIGMA_BUILD_UNIT_TESTS:BOOL=OFF -DENIGMA_BUILD_WRAPPERS_SWIG:BOOL=ON -DWRAP_SWIG_PYTHON:BOOL=ON')
+
 os.system('cmake --build build --config Release')
 
 if not os.path.exists('release'):
@@ -92,10 +98,10 @@ setup = ['import setuptools', \
          '    classifiers=[', \
          '        "Programming Language :: Python :: 3",', \
          '        "License :: OSI Approved :: GNU General Public License (GPL)",', \
-         '        "Operating System :: Microsoft :: Windows",', \
+         '        "Operating System :: Microsoft :: Windows",' if os.name == 'win32' else 'Operating System :: POSIX :: Linux', \
          '    ],', \
          '    python_requires=">=3.6",', \
-         ')']
+         ')']   
 
 with open(pyReleaseFolderTemp + '/setup.py', 'w') as f:
     for l in setup:
@@ -119,23 +125,34 @@ os.system('python setup.py bdist_wheel')
 os.chdir('../../..')
 
 # Python module
-pyReleaseFolder = 'release/ENigMApy_' + strNewVersion + '_win32'
-if not os.path.isdir(pyReleaseFolder):
-    os.mkdir(pyReleaseFolder)
+if os.name == 'win32'
+    pyReleaseFolder = 'release/ENigMApy_' + strNewVersion + '_win32'
+    if not os.path.isdir(pyReleaseFolder):
+        os.mkdir(pyReleaseFolder)
 
-shutil.copy2('build/bin/ENigMApy/dist/ENigMApy-' + strNewVersion + '-py3-none-any.whl', pyReleaseFolder + '/ENigMApy-' + strNewVersion + '-py3-win32.whl')
+    shutil.copy2('build/bin/ENigMApy/dist/ENigMApy-' + strNewVersion + '-py3-none-any.whl', pyReleaseFolder + '/ENigMApy-' + strNewVersion + '-py3-win32.whl')
+else:
+    pyReleaseFolder = 'release/ENigMApy_' + strNewVersion + '_linux'
+    if not os.path.isdir(pyReleaseFolder):
+        os.mkdir(pyReleaseFolder)
+
+    shutil.copy2('build/bin/ENigMApy/dist/ENigMApy-' + strNewVersion + '-py3-none-any.whl', pyReleaseFolder + '/ENigMApy-' + strNewVersion + '-py3-linux.whl')
 
 # CSharp module
-csReleaseFolder = 'release/ENigMAcs_' + strNewVersion + '_win32'
-if not os.path.isdir(csReleaseFolder):
-    os.mkdir(csReleaseFolder)
+if os.name == 'win32':
+    csReleaseFolder = 'release/ENigMAcs_' + strNewVersion + '_win32'
+    if not os.path.isdir(csReleaseFolder):
+        os.mkdir(csReleaseFolder)
 
-shutil.copy2('build/bin/ENigMAcs.dll', csReleaseFolder + '/ENigMAcs.dll')
-shutil.copy2('LICENSE.txt', csReleaseFolder + '/LICENSE.txt')
-shutil.copy2('README.md', csReleaseFolder + '/README.md')
+    shutil.copy2('build/bin/ENigMAcs.dll', csReleaseFolder + '/ENigMAcs.dll')
+    shutil.copy2('LICENSE.txt', csReleaseFolder + '/LICENSE.txt')
+    shutil.copy2('README.md', csReleaseFolder + '/README.md')
 
 # Create tag
-os.system('git commit -a -m v' + strNewVersion)
-os.system('git tag v' + strNewVersion)
-os.system('git push --tags')
+'''
+if incVersion:
+    os.system('git commit -a -m v' + strNewVersion)
+    os.system('git tag v' + strNewVersion)
+    os.system('git push --tags')
+'''
 
